d58f0e27a246bed649243bd485dab4a8c789ae4e
lixing <1147977489@qq.com>
2020-01-14 17:44:30 +0800

    init re

---
+++ src/main/java/com/lx/leeblog/LeeblogApplication.java
@@ -0,0 +1,13 @@
+package com.lx.leeblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeeblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeeblogApplication.class, args);
+    }
+
+}
